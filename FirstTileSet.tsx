9b8d41184f68bb213804017274ec76e1cbdd1a51
Georgi Koemdzhiev <koemdjiev@gmail.com>
2019-07-27 10:42:21 +0100

    Renamed Wizzard to Player

---
+++ FirstTileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="FirstTileSet" tilewidth="16" tileheight="16" tilecount="104" columns="8">
+ <image source="C:/Users/koemd/Downloads/sheet.png" width="128" height="208"/>
+</tileset>
